88e8f541185ca6bc71e848d08593b08832a5b95b
Oery <oery.dev@gmail.com>
2024-10-05 23:25:04 +0200

    Fix Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
-    content: [],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
 	theme: {
     	extend: {
     		borderRadius: {
